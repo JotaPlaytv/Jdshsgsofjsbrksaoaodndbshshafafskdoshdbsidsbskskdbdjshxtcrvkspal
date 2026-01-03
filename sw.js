bbaa610ebcd0e5d563c07ead178233fc0392556b
JotaPlaytv <jota51074@gmail.com>
2026-01-03 13:21:20 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'financeiro-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Instalar Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Ativar Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Removendo cache antigo:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptar requisições
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Retorna do cache se existir
+        if (response) {
+          return response;
+        }
+        // Senão, busca da rede
+        return fetch(event.request);
+      })
+  );
+});
